3a08646d4bb81349674aab1d764186403e8eeb20
gerhynes <gkhynes@gmail.com>
2026-06-03 13:55:32 +0100

    feat: first routes

--- main.py
+++ main.py
@@ -1,8 +1,32 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
+posts = [
+    {
+        "id": 1,
+        "author": "Corey Schafer",
+        "title": "FastAPI is awesome",
+        "content": "This framework is really easy to use and super fast",
+        "date_posted": "April 20, 2025",
+    },
+    {
+        "id": 2,
+        "author": "Jane Doe",
+        "title": "Python is greate for we development",
+        "content": "Python is a great language for web development",
+        "date_posted": "April 21, 2025",
+    },
+]
 
-@app.get("/")
+
+@app.get("/", response_class=HTMLResponse, include_in_schema=False)
+@app.get("/posts", response_class=HTMLResponse, include_in_schema=False)
 def home():
-    return {"message": "Hello World"}
+    return f"<h1>{posts[0]['title']}</h1>"
+
+
+@app.get("/api/posts")
+def get_posts():
+    return posts
